9f34836a903fff1de689a0aeaa5ef2312db28a82
Josemar Alves <henriquejosemar@gmail.com>
2022-09-20 09:51:44 -0300

    Ajustes

--- src/main/java/com/josemar/utils/LocationUtils.java
+++ src/main/java/com/josemar/utils/LocationUtils.java
@@ -4,14 +4,24 @@ import com.josemar.model.Track;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public abstract class LocationUtils {
 
     public static List<Track> removeOutdatedLocations(List<Track> locations, int minusMinutes) {
-        var localDateTimeNowMinus = LocalDateTime.now(Clock.systemUTC()).minusMinutes(minusMinutes);
+        locations = dedupLocations(locations);
 
+        var localDateTimeNowMinus = LocalDateTime.now(Clock.systemUTC()).minusMinutes(minusMinutes);
         return locations.stream().filter(location -> location.getDataHora().isAfter(localDateTimeNowMinus)).toList();
+    }
+
+    private static List<Track> dedupLocations(List<Track> locations) {
+        Set<String> locationSet = new HashSet<>();
+        locations = locations.stream().filter(lo -> locationSet.add(lo.getOrdem())).collect(Collectors.toList());
+        return locations;
     }
 
 }
